73247cf1d3b34c0a1bd98cabcd19cde0733c4c34
Maksim Nikulnikov <5286433@gmail.com>
2025-07-11 22:22:46 +0300

    [FEAT] Add scrollbar.

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,9 @@
+import Scrollbar from "../scrollbar/Scrollbar";
+
 export const Layout = ({ children }) => {
   return (
     <>
+      <Scrollbar/>
       <header>header</header>
       <main>{children}</main>
       <footer>footer</footer>
