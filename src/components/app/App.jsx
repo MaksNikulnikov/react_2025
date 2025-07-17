b2481d95ceac49a272a5e9fb00160bdf7b349a96
Maksim Nikulnikov <5286433@gmail.com>
2025-07-17 17:32:37 +0300

    [FEAT] Add reset.css.

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,8 @@ import { Layout } from "../layout/Layout";
 import { RestaurantsPage } from "../restaurants-page/RestaurantsPage";
 import { restaurants } from "../../assets/mock";
 
+import "./reset.css"
+
 export const App = () => {
   return (
     <Layout>
