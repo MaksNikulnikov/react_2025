e6b9162e3c9480baf372c4bec89355829bb3678b
Maksim Nikulnikov <5286433@gmail.com>
2025-08-03 19:37:02 +0300

    [REFACTOR] Spelling.

--- src/components/restaurant-tab/RestaurantTab.container.jsx
+++ src/components/restaurant-tab/RestaurantTab.container.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { selectRestaurantById } from "../../redux/entities/restoraunts/slice";
+import { selectRestaurantById } from "../../redux/entities/restaurants/slice";
 import { RestaurantTab } from "./RestaurantTab";
 
 export const RestaurantsTabContainer = ({
