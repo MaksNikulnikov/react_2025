ee77eec064afc1355265a3671c80fc7dc9b1fbb0
Maksim Nikulnikov <5286433@gmail.com>
2025-08-04 12:58:38 +0300

    refactor: remove explicit page wrapper components

--- src/pages/menu-page/MenuPage.jsx
+++ src/pages/menu-page/MenuPage.jsx
@@ -1,7 +1,19 @@
 import { useOutletContext } from "react-router";
-import { Menu } from "../../components/menu/Menu";
+import { MenuItemContainer } from "../../components/menu-item/MenuItem.container";
+import styles from "./menu-page.module.css";
 
 export const MenuPage = () => {
   const { menuIds } = useOutletContext();
-  return <Menu menuIds={menuIds} />;
+  return  <>
+        <h3 className={styles.title}>Меню</h3>
+        {menuIds?.length ? (
+          <ul className={styles.list}>
+            {menuIds.map((menuItemId) => (
+              <MenuItemContainer key={menuItemId} menuItemId={menuItemId} />
+            ))}
+          </ul>
+        ) : (
+          <p className={styles.message}>Меню отсутствует</p>
+        )}
+      </>;
 };
