d0fc261190e9c1497f53530110f0b0683d135c61
Maksim Nikulnikov <5286433@gmail.com>
2025-08-04 15:22:38 +0300

    [REFACTOR] Remove explicid component.

--- src/pages/reviews-page/ReviewsPage.jsx
+++ src/pages/reviews-page/ReviewsPage.jsx
@@ -1,7 +1,25 @@
 import { useOutletContext } from "react-router";
-import { Reviews } from "../../components/reviews/Reviews";
+import { useUser } from "../../components/user-context/use-user";
+import styles from "./reviews-page.module.css";
+import { ReviewListItemContainer } from "../../components/review-list-item/ReviewListItem.container";
+import { ReviewForm } from "../../components/review-form/ReviewForm";
 
 export const ReviewsPage = () => {
+  const { isLogged } = useUser();
   const { reviewsIds } = useOutletContext();
-  return <Reviews reviewsIds={reviewsIds} />;
+  return (
+    <section className={styles.reviews}>
+      <h3 className={styles.title}>Отзывы</h3>
+      {reviewsIds?.length ? (
+        <ul className={styles.list}>
+          {reviewsIds.map((reviewId) => (
+            <ReviewListItemContainer key={reviewId} reviewId={reviewId} />
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.message}>Отзывов пока нет</p>
+      )}
+      {isLogged && <ReviewForm />}
+    </section>
+  );
 };
