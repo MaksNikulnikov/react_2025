1ab7c067de287a2c1e942cc076724b5d62796ce9
Maksim Nikulnikov <5286433@gmail.com>
2025-08-05 14:01:21 +0300

    feat: add user info component

--- src/components/review-list-item/ReviewListItem.jsx
+++ src/components/review-list-item/ReviewListItem.jsx
@@ -1,11 +1,14 @@
 import { Counter } from "../counter/Counter";
+import { UserInfo } from "../user-info/UserInfo";
 import styles from "./review-list-item.module.css";
 
 export const ReviewListItem = ({ review }) => {
+  console
   return (
-    <li className={styles.item}>
-      {review.text}
-      <Counter />
+    <li className={styles.reviewItem}>
+      <p className={styles.reviewText}>{review.text}</p>
+      <Counter value={review.rating}/>
+      <UserInfo userId={review.userId} />
     </li>
   );
 };
