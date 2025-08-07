c5f316040d065596e5c6a70d632b0a307781ec50
Maksim Nikulnikov <5286433@gmail.com>
2025-08-07 10:12:33 +0300

    [REFACTOR] Update default counter's hooks.

--- src/components/default-counter/hooks.jsx
+++ src/components/default-counter/hooks.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import { useState } from "react";
+
 
 export const useCounter = ({
   min = -Infinity,
   max = Infinity,
   initial = 0,
 }) => {
-  const [count, setCount] = React.useState(initial);
+  const [count, setCount] = useState(initial);
   return {
     count,
     increment: () => setCount((prevCount) => Math.min(max, prevCount + 1)),
