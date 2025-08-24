5ffa98c4e91162662515b8bbe17db59f64167d45
Maksim Nikulnikov <5286433@gmail.com>
2025-08-24 08:44:51 +0300

    [FEAT] Add modal.

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Button } from "../button/Button";
+import { createPortal } from "react-dom";
+import styles from "./modal.module.css"
+
+export const Modal = ({ buttonName, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setIsVisible(!isVisible)} name={buttonName} />
+      {isVisible &&
+        createPortal(
+          <div className={styles.modal_backdrop} onClick={() => setIsVisible(false)}>
+            <div className={styles.modal_content} onClick={(e) => e.stopPropagation()}>
+              {children}
+            </div>
+          </div>,
+          document.getElementById("modal")
+        )}
+    </>
+  );
+};
